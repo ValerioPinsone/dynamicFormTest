3d13cd62e9053cda200eca11cd4b91e15764ceb2
Valerio Pinsone <valerio.pinsone@gmail.com>
2023-05-16 16:10:02 +0200

    first setup no validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { JsonFormData } from './components/json-form/json-form.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'dynamicFormTest';
+  public formData!: JsonFormData;
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    console.log("ngoninit");
+    this.http.get('../assets/my-form.json').subscribe((formData) => {
+        this.formData = formData as JsonFormData;
+        console.log(this.formData);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JsonFormComponent } from './components/json-form/json-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JsonFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
